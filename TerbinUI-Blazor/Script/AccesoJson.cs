846e1443f3ffb7fed8cbe2296c431811b6a210ca
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-09-24 18:43:12 +0200

    Configuracion + Idiomas + AccesoJson + Instancia + Mods

---
+++ TerbinUI-Blazor/Script/AccesoJson.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+namespace Sui.AccesoDatos
+{
+    public static class AccesoJson
+    {
+        // ***********************( Funciones )*********************** //
+        public static (bool succes, string menssage)
+            AccesoNormal(string eDirectory, string eArchivo, out Dictionary<string, string> exDiccionario)
+        {
+            exDiccionario = new();
+
+            string rutaCompleta = Path.Combine(eDirectory, eArchivo);
+            if (!File.Exists(rutaCompleta))
+                return (false, $"No se encontró el archivo: {rutaCompleta}");
+
+            string? archivo = File.ReadAllText(rutaCompleta);
+            if (archivo == null)
+                return (false, $"Archivo es Null: {rutaCompleta}");
+
+            exDiccionario = JsonSerializer.Deserialize<Dictionary<string, string>>(archivo) ?? new();
+            if (exDiccionario == null)
+                return (false, $"No se pudo deserializar el archivo: {rutaCompleta}");
+
+            return (true, string.Empty);
+        }
+
+        /*
+        // TODO: Reacer para Unity.
+        public static string AccesoUnity(string _rutaArchivo_s, string _archivo_s)
+        {
+            string _rutaCompleta_s = Path.Combine(_rutaArchivo_s, Path.GetFileNameWithoutExtension(_archivo_s));
+            _rutaCompleta_s = _rutaCompleta_s.Replace("\\", "/");
+            TextAsset _assetJSon = Resources.Load<TextAsset>(_rutaCompleta_s);
+
+            if (_assetJSon == null)
+            {
+                Debug.LogWarning($"(f_accesoJson_s): No se encontró el archivo en Resources: {_rutaCompleta_s}");
+                return null;
+            }
+            return _assetJSon.text;
+        }*/
+    }
+}
--- TerbinUI-Blazor/Script/ManageConfiguracion.cs
+++ TerbinUI-Blazor/Script/ManageConfiguracion.cs
@@ -1,7 +1,56 @@
-﻿namespace TerbinUI_Blazor.Script
+﻿using Sui.AccesoDatos;
+
+namespace TerbinUI_Blazor.Script
 {
-    public class ManageConfiguracion
+    public static class ManageConfiguracion
     {
+        // ***********************( Variables )*********************** //
+        private const string _filePath = "wwwroot\\Data\\";
+        private const string _fileName = "config.json";
+
+        private static Dictionary<string, string>? _config;
+
+        private const string _pathInstancias = "instancias";
+        private const string _pathMods = "mods";
+        private const string _language = "idioma";
+        private const string _apiKey = "apiKey";
+
+        // ***********************( GSI )*********************** //
+        public static Dictionary<string, string>? Config
+        {
+            get
+            {
+                // TODO: Gestionar.
+                return _config;
+            }
+        }
+
+        //private static string 
+
+        // ***********************( Funciones )*********************** //
+        private static bool existsConfig()
+        {
+            return File.Exists(Path.Combine(_filePath, _fileName));
+        }
+
+        private static void crearConfigPorDefecto()
+        {
+            var datos = new Dictionary<string, string>()
+            {
+                { _pathInstancias, "" },
+                { _pathMods, ""},
+                { _language, "English.json"},
+                { _apiKey, "-1"}
+            };
+        }
+
+        private static void handleArchivo()
+        {
+            if (!existsConfig())
+            {
+                crearConfigPorDefecto();
+            }
+        }
 
     }
 }
