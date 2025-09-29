9cb2c90e65e012d1b49ffd0a49bb1886eed48482
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-09-29 11:12:54 +0200

    woker xd

---
+++ TerbinUI-Blazor/Script/Backend/PruebasWoeker.cs
@@ -0,0 +1,37 @@
+﻿using System.IO.Pipes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TerbinUI_Blazor.Script.Backend
+{
+    public class PruebasWoeker
+    {
+    }
+
+    public class SumWorker
+    {
+        public async Task IniciarAsync()
+        {
+            using var server = new NamedPipeServerStream
+            (
+                "SumaPipe",
+                PipeDirection.InOut,
+                1,
+                PipeTransmissionMode.Byte,
+                PipeOptions.Asynchronous
+            );
+            while (true)
+            {
+                await server.WaitForConnectionAsync();
+
+                byte[] buffer = new byte[2]; // Espera dos bytes (sumando y sumando)
+                int bytesRead = await server.ReadAsync(buffer, 0, buffer.Length);
+
+                byte resultado = (byte)(buffer[0] + buffer[1]);
+                await server.WriteAsync(new byte[] { resultado }, 0, 1);
+            }
+        }
+    }
+
+}
