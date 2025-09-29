81daa3f0eb3400385926f1806666535f08896a7d
Sui <abravolozano@safareyes.es>
2025-09-29 08:52:33 +0200

    Create WorkerSimple.cs

---
+++ TerbinUI-Blazor/Script/Backend/WorkerSimple.cs
@@ -0,0 +1,25 @@
+﻿
+namespace TerbinUI_Blazor.Script.Backend
+{
+    public class WorkerSimple : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        private float sum(float[] eNum)
+        {
+            float resultado = 0;
+            for (int i = 0; i < eNum.Length; i++)
+            {
+                resultado += eNum[i];
+            }
+            return resultado;
+        }
+    }
+}
